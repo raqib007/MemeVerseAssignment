4c3f408a8257f5aba1f05dfac060e4296d403111
raqib007 <rhsabbir@yahoo.com>
2021-05-29 14:46:19 +0600

    Refactored

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "./config/config.env" });
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 const connectDB = require("./config/db");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
@@ -12,7 +13,7 @@ const routes = require('./routes');
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
 app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -26,3 +27,9 @@ server.listen(PORT, () => {
 });
 
 
+// app.use(express.static(path.join(__dirname, 'build')));
+
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
